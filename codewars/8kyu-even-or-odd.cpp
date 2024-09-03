5e63e5e942e0e29a1d1376f6b5cf17c018558b7a
Kyle Curtis <30717363+kylecurtis@users.noreply.github.com>
2024-09-03 09:55:22 -0400

    Add even or odd solution

---
+++ codewars/8kyu-even-or-odd.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <cassert>
+
+/* SOLUTION
+======================================================================= */
+std::string even_or_odd(int number)
+{
+    if (number % 2 == 0) {
+        return "Even";
+    }
+
+    return "Odd";
+}
+
+/* TEST
+======================================================================= */
+void test() {
+    // Test case: zero
+    assert(even_or_odd(0) == "Even");
+
+    // Test cases: positive odd numbers
+    assert(even_or_odd(1) == "Odd");
+    assert(even_or_odd(21) == "Odd");
+
+    // Test cases: positive even numbers
+    assert(even_or_odd(2) == "Even");
+    assert(even_or_odd(100) == "Even");
+
+    // Test cases: negative odd numbers
+    assert(even_or_odd(-1) == "Odd");
+    assert(even_or_odd(-21) == "Odd");
+
+    // Test cases: negative even numbers
+    assert(even_or_odd(-2) == "Even");
+    assert(even_or_odd(-100) == "Even");
+
+    std::cout << "All tests passed!" << std::endl;
+}
+
+/* MAIN
+======================================================================= */
+int main() {
+    test();  // Run the tests
+    return 0;
+}
